60dc68d9e582ab6cc799f1e54813f7b021a7bfd8
Coder Gautam <gautamgxtv@gmail.com>
2022-06-27 06:25:33 -0500

    healthbar

---
+++ src/components/HealthBar.ts
@@ -0,0 +1,88 @@
+import Phaser from "phaser";
+class HealthBar extends Phaser.GameObjects.Container {
+    custom: boolean;
+    bar: Phaser.GameObjects.Graphics;
+    maxValue: number;
+    value: number;
+    toLerp: number;
+
+    constructor (game: Phaser.Scene, x: number, y: number, width: number, height: number, custom=false)
+    {
+      
+
+        super(game);
+
+        this.custom = custom;
+        this.bar = new Phaser.GameObjects.Graphics(game).setDepth(99);
+  
+        this.x = x;
+        this.y = y;
+
+        this.maxValue = 100;
+        this.value = 0;
+        this.toLerp = 0;
+        this.height = height;
+        this.width = width;
+  
+        this.add(this.bar);
+        game.add.existing(this);
+    }
+  
+    setHealth (amount: number)
+    {
+  
+        const clamp = (num: number, min: number, max: number) => Math.min(Math.max(num, min), max);
+        this.value = clamp(amount, 0, this.maxValue);
+  
+        this.draw();
+  
+        return (this.value === 0);
+    }
+    setLerpValue(value: any) {
+        this.toLerp = value;
+    }
+    draw ()
+    {
+        this.bar.clear();
+  
+        //  BG
+        this.bar.fillStyle(0x000000);
+        this.bar.fillRect(this.x, this.y, this.width, this.height);
+  
+        //  Health
+  
+        this.bar.fillStyle(0xffffff);
+        this.bar.fillRect(this.x + 2, this.y + 2, this.width-4, this.height-4);
+  
+        if(!this.custom) {
+        if (this.value/this.maxValue < 0.30)
+        {
+            this.bar.fillStyle(0xff0000);
+        }
+        else if(this.value/this.maxValue  < 0.5) {
+           this.bar.fillStyle(0xFFFF00); 
+        } else
+        {
+            this.bar.fillStyle(0x00ff00);
+        }
+    } else {
+
+        this.bar.fillStyle(0x00FFFF);
+    }
+
+        var d = Math.floor((this.width-4) * (this.value/this.maxValue));
+  
+        this.bar.fillRect(this.x + 2, this.y + 2, d, this.height-4);
+    }
+
+    updateContainer() {
+        const lerp = (start: number,end: number,amt: number) => (1-amt)*start+amt*end;
+
+        this.setHealth(lerp(this.value, this.toLerp, 0.25));
+       
+    }
+  
+  }
+  
+
+  export default HealthBar;
