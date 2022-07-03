df112162b6ec78d7833ea5aaa40f48a37e00e356
Coder Gautam <gautamgxtv@gmail.com>
2022-07-02 22:40:17 -0500

    E

--- helpers/levels.js
+++ helpers/levels.js
@@ -1,5 +1,5 @@
-// const levels = [50, 100, 200, 500, 750, 1000, 2000, 3000, 4000, 5000];
+const levels = [50, 100, 200, 500, 750, 1000, 2000, 3000, 4000, 5000];
 // debugging levels
-const levels = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const levels = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 module.exports = levels;
